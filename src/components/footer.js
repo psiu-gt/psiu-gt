41fce382852971faede4d87b2702dcf9f1ed1d25
Drake Witt <dwitt@pop-os.localdomain>
2020-08-11 20:10:16 -0400

    Add social icon links.

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,6 +2,32 @@ import React from 'react'
 
 const Footer = ({ content }) => (
   <footer className="section site-footer">
+    <div className="social-icons is-centered">
+      <a
+        href="https://www.facebook.com/psiugt/"
+        className="button is-medium is-facebook"
+      >
+        <span className="icon">
+          <i className="fa fa-facebook"></i>
+        </span>
+      </a>
+      <a
+        href="https://www.instagram.com/gtpsiu"
+        className="button is-medium is-instagram"
+      >
+        <span className="icon">
+          <i className="fa fa-instagram"></i>
+        </span>
+      </a>
+      <a
+        href="https://twitter.com/psiugt"
+        className="button is-medium is-twitter"
+      >
+        <span className="icon">
+          <i className="fa fa-twitter"></i>
+        </span>
+      </a>
+    </div>
     <div
       className="container has-text-centered"
       dangerouslySetInnerHTML={{ __html: content }}
